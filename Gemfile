9e8c675fbf45146ea6fdd424474fe138091bd70b
Justin Louie <jslouie@gmail.com>
2016-11-28 17:21:28 +0800

    HHAHAHAHAHAH

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'https://rubygems.org'
+
+gem 'action_cable_client'
+gem 'any_bar'
---
+++ main.rb
@@ -0,0 +1,24 @@
+require 'rubygems'
+
+require 'action_cable_client'
+require 'any_bar'
+
+any_bar = AnyBar::Client.new(1738)
+
+EventMachine.run do
+  uri = "ws://toilet.guavawesome.com/websocket"
+  client = ActionCableClient.new(uri, 'DoorEventChannel')
+  # the connected callback is required, as it triggers
+  # the actual subscribing to the channel but it can just be
+  # client.connected {}
+  client.connected { puts 'successfully connected.' }
+
+  # called whenever a message is received from the server
+  client.received do | data |
+    color = data.try(:[], 'message').try(:[], 'color')
+
+    any_bar.color = color || 'black'
+  end
+
+  client.disconnected { any_bar.color = 'grey' }
+end
